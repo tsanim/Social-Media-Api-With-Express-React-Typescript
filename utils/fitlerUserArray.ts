a0054081bbd3dc92592c8894635ab46b8fa039ee
tsanim <cancy@abv.bg>
2020-01-10 15:26:58 +0200

    Add tyoes to params

--- utils/fitlerUserArray.ts
+++ utils/fitlerUserArray.ts
@@ -1,4 +1,6 @@
-export default function filterUserArray(userArr, userId) {
+import IUser from "../interfaces/models/User.interface";
+
+export default function filterUserArray(userArr: IUser[], userId: string) {
     if (userArr.findIndex(obj => obj._id.toString() === userId) >= 0) {
         return userArr.filter(u => u._id.toString() !== userId);
     }
--- utils/pushInUserarray.ts
+++ utils/pushInUserarray.ts
@@ -1,5 +1,7 @@
+import IUser from "../interfaces/models/User.interface";
+
 //func to push in user's array of followers
-export default function pushInUserArray(userArr, user) {
+export default function pushInUserArray(userArr: IUser[], user: IUser) {
     if (userArr.findIndex(obj => obj._id.toString() === user._id.toString()) === -1) {
         delete user.hashedPassword;
         delete user.salt;
