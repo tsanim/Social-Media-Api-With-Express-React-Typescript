58061449ccbf7bc4d350429d6337f06b8817ce9c
tsanim <cancy@abv.bg>
2020-01-10 12:39:37 +0200

    Add Comments, Users, File Controller

--- app.ts
+++ app.ts
@@ -5,15 +5,21 @@ import App from './App/App';
 import AuthController from './controllers/AuthController';
 import PostsController from './controllers/PostsController';
 import storage from './config/gridFsStorage';
+import CommentsController from './controllers/CommentsController';
+import FileController from './controllers/FileController';
+import UsersController from './controllers/UsersController';
 
-// //init env 
+//init env 
 const env = process.env.NODE_ENV || 'development';
 
 const config: BaseConfig = generalConfig[env];
 
 const app = new App([
     new AuthController(),
-    new PostsController(storage)
+    new PostsController(storage),
+    new CommentsController(),
+    new UsersController(storage),
+    new FileController()
 ], config, logger);
 
 app.listen();
