29a0be02d9af30f43c89d031835d4cc81a86b91f
tsanim <cancy@abv.bg>
2020-01-13 18:26:19 +0200

    Add semicolon

--- interfaces/models/UserModel.ts
+++ interfaces/models/UserModel.ts
@@ -2,5 +2,5 @@ import { Model } from "mongoose";
 import IUser from "./User.interface";
 
 export default interface UserModel extends Model<IUser> {
-    seedAdmin: () => Promise<IUser>
+    seedAdmin: () => Promise<IUser>;
 }
