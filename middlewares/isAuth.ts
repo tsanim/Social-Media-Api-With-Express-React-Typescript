214991f490a399ed62ed1176d12cac09c81de73d
tsanim <cancy@abv.bg>
2020-01-14 12:59:19 +0200

    Add DecodedToken and RequestCustom interfaces

--- middlewares/isAuth.ts
+++ middlewares/isAuth.ts
@@ -3,11 +3,13 @@ import jwt from 'jsonwebtoken';
 import Configuration from '../config/Configuration';
 //init logger
 import logger from '../logger/logger'
+import DecodedToken from '../interfaces/DecodedToken.interfaca';
+import RequestCustom from '../interfaces/RequestCustom.interface';
 
 const env = process.env.NODE_ENV || 'development';
 const config = new Configuration(env);
 
-export default  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+export default  (req: RequestCustom, res: express.Response, next: express.NextFunction) => {
   //get request header for authorization
   const authHeaders = req.get('Authorization');
 
@@ -23,11 +25,11 @@ export default  (req: express.Request, res: express.Response, next: express.Next
   const token = req.get('Authorization').split(' ')[1];
 
   //init decodedToken
-  let decodedToken;
+  let decodedToken: DecodedToken;
 
   try {
     //verify token from header so we can see if user is authenticated
-    decodedToken = jwt.verify(token, config.environmentConfig.JWT_SECRET);
+    decodedToken = jwt.verify(token, config.environmentConfig.JWT_SECRET) as DecodedToken;
   } catch (error) {
     logger.log('error', `Token is invalid!`);
 
@@ -42,6 +44,8 @@ export default  (req: express.Request, res: express.Response, next: express.Next
     return res.status(401)
       .json({ message: 'Not authenticated.' });
   }
+
+  console.log(decodedToken);
 
   //set user id to req from decoded token
   req.userId = decodedToken.userId;
